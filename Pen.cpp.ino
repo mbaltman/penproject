d84f4cb8fd5d6fbe4f686b251b8aeef17138c03f
mbaltman <mbaltman@cps.edu>
2019-10-07 12:46:15 -0500

    Added Files, fixed it lol
    noice

---
+++ Pen.cpp.ino
@@ -0,0 +1,23 @@
+#include "Arduino.h"
+#include "Pen.h"
+#include "alphabet.h"
+
+Pen::Pen()
+{
+  
+}
+
+void Pen:: writeLetter(char letter)
+{
+  
+}
+
+void Pen::movePen(boolean penPosition)
+{
+  
+}
+
+void Pen:: drawLine(double startx, double starty, double endx, double endy, boolean penPosition)
+{
+  movPen(penPosition)
+}
